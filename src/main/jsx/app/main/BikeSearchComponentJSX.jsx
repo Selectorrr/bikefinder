29bb77d880e3cc19a401c73013b487c65a7c3bc7
Stepan Litvinov <litvinovsd@gmail.com>
2016-03-27 01:45:58 +0300

    more completely example  for search kit

--- src/main/jsx/app/main/BikeSearchComponentJSX.jsx
+++ src/main/jsx/app/main/BikeSearchComponentJSX.jsx
@@ -1,13 +1,9 @@
 /**
  * Created by Selector on 26.03.2016.
  */
-// const host = "http://demo.searchkit.co/api/movies";
-const host = "api/users";
+const host = "http://demo.searchkit.co/api/movies";
+// const host = "api/users";
 const sk = new Searchkit.SearchkitManager(host, {});
-const SearchkitProvider = Searchkit.SearchkitProvider;
-const Searchbox = Searchkit.SearchBox;
-
-const Hits = Searchkit.Hits;
 
 sk.translateFunction = (key) => {
     let translations = {
@@ -22,12 +18,17 @@ class MovieHit extends React.Component {
         const result = this.props.result;
         let url = "http://www.imdb.com/title/" + result._source.imdbId;
         return (
-            <div className={this.props.bemBlocks.item().mix(this.props.bemBlocks.container("item"))} key={result._id}>
-                <a href={url} target="_blank">
-                    <img className={this.props.bemBlocks.item("poster")} src={result._source.poster} width="100"
-                         height="140"/>
-                    <div className={this.props.bemBlocks.item("title")}>{result._source.title}</div>
-                </a>
+            <div>
+                <div className="example-hits-hit example-hits__item">
+                    <a href={url} target="_blank">
+                        <img className="example-hits-hit__poster"
+                             src={result._source.poster}
+                             width="180" height="270"/>
+                    </a>
+                    <a href={url} target="_blank">
+                        <div className="example-hits-hit__title">{result._source.title}</div>
+                    </a>
+                </div>
             </div>
         )
     }
@@ -36,22 +37,54 @@ class MovieHit extends React.Component {
 var BikeSearchComponent = React.createClass({
     render: function () {
         return (
-            <SearchkitProvider searchkit={sk}>
-                <div className="sk-layout">
+            <Searchkit.SearchkitProvider searchkit={sk}>
+                <div className="sk-layout example-search-site">
+
                     <div className="sk-layout__top-bar sk-top-bar">
                         <div className="sk-top-bar__content">
-                            <Searchbox searchOnChange={true}
-                                       prefixQueryFields={["actors^1","type^2","languages","title^10"]}/>
+                            <Searchkit.SearchBox searchOnChange={true}
+                                                 prefixQueryFields={["actors^1","type^2","languages","title^10"]}/>
                         </div>
                     </div>
+
                     <div className="sk-layout__body">
+
+                        <div className="sk-layout__filters">
+                            <Searchkit.RangeFilter field="metaScore" id="metascore" min={0} max={100}
+                                                   showHistogram={true} title="MetaScore"/>
+                            <Searchkit.HierarchicalMenuFilter
+                                fields={["categories_lvl1", "categories_lvl2", "categories_lvl3"]} title="Categories"
+                                id="categories"/>
+                            <Searchkit.RefinementListFilter id="actors" title="Actors" field="actors.raw"
+                                                            operator="AND"/>
+                        </div>
+
                         <div className="sk-layout__results sk-results-list">
+
                             <div className="sk-results-list__action-bar sk-action-bar">
+                                <div className="sk-action-bar__info">
+                                    <Searchkit.HitsStats />
+                                    <Searchkit.ViewSwitcherToggle/>
+
+                                </div>
+
+                                <div className="sk-action-bar__filters">
+
+                                    <Searchkit.SelectedFilters/>
+                                    <Searchkit.ResetFilters/>
+                                </div>
+
                             </div>
-                            <Hits hitsPerPage={6} itemComponent={MovieHit}/></div>
+                            <div className="example-search-site__results">
+                                <Searchkit.Hits hitsPerPage={20} itemComponent={MovieHit} className="example-hits"/>
+                                <Searchkit.NoHits/>
+                                <Searchkit.Pagination />
+                            </div>
+
+                        </div>
                     </div>
                 </div>
-            </SearchkitProvider>);
+            </Searchkit.SearchkitProvider>);
     }
 });
 angular.module('bikefinderApp')
